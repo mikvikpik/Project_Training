58b546a94e29ea6ce5efc3eac263880541960837
Andrew Mikol <andrewmikol@yahoo.com>
2020-01-19 17:39:35 -0600

    January 19 2020 Python Crash Course Practice

---
+++ making_pizzas.py
@@ -0,0 +1,9 @@
+"""Importing modules from other files"""
+
+# import pizza_func file for making pizzas)
+
+import pizza_func
+
+# call the make_pizza function from the pizza_func file
+pizza_func.make_pizza(16, 'pepperoni')
+pizza_func.make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
