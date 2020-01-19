39eba9b567f0b9a62d25d9a98574ec39907bda8e
Andrew Mikol <andrewmikol@yahoo.com>
2020-01-18 20:27:49 -0600

    January 18 2020 Python Crash Course Book Practice

--- person.py
+++ person.py
@@ -7,3 +7,15 @@ def build_person(first_name, last_name):
 
 musician = build_person('jimi', 'hendrix')
 print(musician)
+
+# Return dictionary with age
+
+def build_person(first_name, last_name, age =''):
+    """Return a dictionary of information about a person."""
+    person = {'first': first_name, 'last': last_name}
+    if age:
+        person['age'] = age
+    return person
+
+musician = build_person('jimi', 'hendrix', age = 27)
+print(musician)
