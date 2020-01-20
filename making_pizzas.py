b06fe6de432333d0e9c11b47484b6496bc0b7c14
Andrew Mikol <andrewmikol@yahoo.com>
2020-01-20 13:03:35 -0600

    January 20, 2020 Practicing Python Crash Course

--- making_pizzas.py
+++ making_pizzas.py
@@ -7,3 +7,16 @@ import pizza_func
 # call the make_pizza function from the pizza_func file
 pizza_func.make_pizza(16, 'pepperoni')
 pizza_func.make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
+
+""" Importing using from module import function"""
+
+from pizza_func import make_pizza
+
+make_pizza(17, 'pepperoni', 'sausage')
+
+
+"""Importing using alias"""
+
+from pizza_func import make_pizza as mp
+
+mp(18, 'green peppers', 'mushrooms', 'extra cheese')
