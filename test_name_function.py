86a85ada7a5fc001035742cb39ec245708771e08
Andrew Mikol <andrewmikol@yahoo.com>
2020-01-23 11:36:42 -0600

    January 23 2020 Python Crash Course Practice

--- test_name_function.py
+++ test_name_function.py
@@ -9,4 +9,10 @@ class NamesTestCase(unittest.TestCase):
         formatted_name = get_formatted_name('janis', 'joplin')
         self.assertEqual(formatted_name, 'Janis Joplin')
 
+    def test_first_last_middle_name(self):
+        """Do names like 'Wolfgang Amadeus Mozart' work?"""
+        formatted_name = get_formatted_name(
+            'wolfgang', 'mozart', 'amadeus')
+        self.assertEqual(formatted_name, 'Wolfgang Amadeus Mozart')
+        
 unittest.main()
