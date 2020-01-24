cf1703a397815ebba839886497fb1fa43649bf76
Andrew Mikol <andrewmikol@yahoo.com>
2020-01-24 11:01:46 -0600

    January 24 2020 Python Crash Course Project Alien Invasion

---
+++ alien_invasion.py
@@ -0,0 +1,28 @@
+import sys
+
+import pygame
+
+from settings import settings
+def run_game():
+    # Initialize pygame, settings, and screen object.
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+
+    # Start the main loop for the game.
+    while True:
+
+        # Watch for the keyboard and mouse events.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        # Redraw the screen during each pass through the loop.
+        screen.fill(ai_settings.bg_color)
+
+        # Make the most recently drawn screen visible.
+        pygame.display.flip()
+
+run_game()
---
+++ settings.py
@@ -0,0 +1,10 @@
+class Settings():
+    """A class to store all settings in Alien Invasion."""
+
+    def __init__(self):
+        """Initialize the game's settings."""
+        # Screen Settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+        
