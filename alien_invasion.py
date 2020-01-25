a1ad479e8f3ce87d1e49d34a72d88f4b83d0626e
Andrew Mikol <andrewmikol@yahoo.com>
2020-01-25 13:09:21 -0600

    January 25 2020 Python Crash Course Project

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,8 +1,8 @@
 import sys
-
 import pygame
-
 from settings import settings
+from ship import Ship
+
 def run_game():
     # Initialize pygame, settings, and screen object.
     pygame.init()
@@ -10,6 +10,9 @@ def run_game():
     screen = pygame.display.set_mode(
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+
+    # Make a ship.
+    ship = Ship(screen)
 
     # Start the main loop for the game.
     while True:
@@ -21,7 +24,8 @@ def run_game():
 
         # Redraw the screen during each pass through the loop.
         screen.fill(ai_settings.bg_color)
-
+        ship.blitme()
+        
         # Make the most recently drawn screen visible.
         pygame.display.flip()
 
