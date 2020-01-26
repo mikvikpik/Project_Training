92b66d84fe7e9d57f05812dcf19e6b5318b236b8
Andrew Mikol <andrewmikol@yahoo.com>
2020-01-26 15:52:36 -0600

    January 26, 2020 Python Crash Course Project

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+import game_functions as gf
 
 def run_game():
     # Initialize pygame, settings, and screen object.
@@ -16,17 +17,7 @@ def run_game():
 
     # Start the main loop for the game.
     while True:
-
-        # Watch for the keyboard and mouse events.
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-
-        # Redraw the screen during each pass through the loop.
-        screen.fill(ai_settings.bg_color)
-        ship.blitme()
-
-        # Make the most recently drawn screen visible.
-        pygame.display.flip()
+        gf.check_events()
+        gf.update_screen(ai_settings, screen, ship)
 
 run_game()
